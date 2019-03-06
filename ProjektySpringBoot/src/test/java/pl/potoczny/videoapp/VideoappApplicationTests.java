f77b00cfc7d7f8c99a2ec1235b07cbfe2c4b249d
marpot <33203978+marpot@users.noreply.github.com>
2019-03-06 11:47:23 +0100

    Add files via upload

---
+++ ProjektySpringBoot/src/test/java/pl/potoczny/videoapp/VideoappApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.potoczny.videoapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
