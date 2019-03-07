622d01ff3e6513e4cb6272e2e72b14b3e8f1f660
marpot <marcin.potoczny@protonmail.com>
2019-03-07 12:01:31 +0100

    dsa
    Signed-off-by: marpot <marcin.potoczny@protonmail.com>

--- SpringApp/ProjektySpringBoot/src/main/java/pl/potoczny/videoapp/VideoappApplication.java
+++ SpringApp/ProjektySpringBoot/src/main/java/pl/potoczny/videoapp/VideoappApplication.java
@@ -2,7 +2,7 @@ package pl.potoczny.videoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//siema
 @SpringBootApplication
 public class VideoappApplication {
 
